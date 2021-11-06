c94565e23267085fd8fabcefbfeebbc27793422f
Matias <matiasmilito99@gmail.com>
2021-11-06 15:15:05 -0300

    .

--- pw-tup/src/App.js
+++ pw-tup/src/App.js
@@ -14,22 +14,26 @@ function App() {
     <div className="App">
       <div>
         <header className="App-header">
-          <img src={pgimg} className="logo" alt="logo"/>
-          <nav>
-            <ul className="navbar">
-              <li><a href="#">Inicio</a></li>
-              <li><a href="#">Nosotros</a></li>
-              <li><a href="#">Cervezas</a>
-                <ul>
-                  <li><a href="#">Lager</a></li>
-                  <li><a href="#">Amber</a></li>
-                  <li><a href="#">IPA</a></li>
-                  <li><a href="#">Stout</a></li>
-                </ul>
-              </li>
-            </ul>
-          </nav>
-          <a href="#"><button>Registrarme</button></a>
+          <div className="sep">
+            <img src={pgimg} className="logo" alt="logo"/>
+            <nav>
+              <ul className="navbar">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Nosotros</a></li>
+                <li><a href="#">Cervezas</a>
+                  <ul>
+                    <li><a href="#">Rubias</a></li>
+                    <li><a href="#">Rojas</a></li>
+                    <li><a href="#">Negras</a></li>
+                    <li><a href="#">IPA</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div>
+            <a href="#"><button>Registrarme</button></a>
+          </div>
         </header>
       </div>
     </div>
