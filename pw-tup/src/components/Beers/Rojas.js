d6969e9c3a2ac07c56f758cf8d7675fedbd5bdda
Matias <matiasmilito99@gmail.com>
2021-11-09 19:20:20 -0300

    Creacion de producto react
    creacion del contenedor del producto, falta terminar de pulirlo

--- pw-tup/src/components/Beers/Rojas.js
+++ pw-tup/src/components/Beers/Rojas.js
@@ -1,10 +1,21 @@
 import "./Rojas.css";
+import pgimg from "../../images/logo1.png";
 
 const Rojas = () => {
     return (
         <div>
             <div className="red-container">
                 <h1 className="red-title">ROJAS</h1>
+            </div>
+            <div>
+                <div className="product-container">
+                    <img src={pgimg} className="img-product" alt="logo"/>
+                    <h2 className="product-title">Andes Roja</h2>
+                    <h2 className="product-price">$100.00</h2>
+                    <label className="product-cantidad">Cantidad</label>
+                    <input type="number" className="product-box"/>
+                    <button className="product-button">Añadir</button>
+                </div>
             </div>
         </div>
     );
