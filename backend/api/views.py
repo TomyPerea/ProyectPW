efdfddd2b9770a5bc8e021378642ec74c6dcaeed
Matias <matiasmilito99@gmail.com>
2021-11-17 13:54:41 -0300

    register, me

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets, generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from api.models import Beers
-from api.serializers import BeersSerializer, RegisterSerializer
+from api.serializers import BeersSerializer, RegisterSerializer, MeSerializer
 
 
 class BeersViewSet(viewsets.ModelViewSet):
@@ -14,3 +17,10 @@ class BeersViewSet(viewsets.ModelViewSet):
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def me(request):
+    print(MeSerializer(request.user).data)
+    return Response(MeSerializer(request.user).data, 200)
