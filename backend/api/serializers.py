7684564bc94b26e3d58c5da5c9bb7e575525d57d
Matias <matiasmilito99@gmail.com>
2021-11-18 20:40:00 -0300

    Backend

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -9,19 +9,26 @@ class BeersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Beers
         fields = "__all__"
+        
+    def create(self, validated_data):
+        beer = super(BeersSerializer, self).create(validated_data)
+        return beer
 
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    first_name = serializers.CharField(max)
 
     class Meta:
         model = get_user_model()
-        fields = ["username", "password"]
+        fields = ["username", "password", "first_name", "last_name"]
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(
             username=validated_data['username'],
             password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         return user
 
--- backend/api/views.py
+++ backend/api/views.py
@@ -14,6 +14,13 @@ class BeersViewSet(viewsets.ModelViewSet):
     serializer_class = BeersSerializer
     queryset = Beers.objects.all()
 
+    def get_permissions(self):
+        if self.action == 'create':
+            self.permission_classes = [IsAuthenticated]
+        else:
+            self.permission_classes = []
+        return super(BeersViewSet, self).get_permissions()
+
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
