040f6daf3be020eac16996c3ed966f253ec8cbcb
Tomas <tomyestepa0@gmail.com>
2021-11-29 20:45:18 -0300

    cambios

--- pw-tup/src/components/Profile/Profile.js
+++ pw-tup/src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
 import {useEffect, useState} from "react";
 import {httpGet} from "../../Utils/httpFunctions";
+import "./Profile.css"
 
 
 const Profile = () => {
@@ -11,12 +12,15 @@ const Profile = () => {
     httpGet('api/me/').then((res) => setUserData(res.data))
   }, [])
 
-  return <div>
-    <h2>Hola! Este es mi perfil</h2>
-    <h3>Mi nombre y apellido es {userData.first_name} {userData.last_name}</h3>
-    <h3>Mi correo es {userData.email}</h3>
-    <h3>Mi nombre de usuario es {userData.username}</h3>
+  return <div className="container">
+    <div className="container-box">
+      <h2 className="box">Hola! Bienvenido a tu perfil</h2>
+      <h3 className="box">Mi nombre y apellido es {userData.first_name} {userData.last_name}</h3>
+      <h3 className="box">Mi correo es {userData.email}</h3>
+      <h3 className="box">Mi nombre de usuario es {userData.username}</h3>
+    </div>
   </div>
+
 }
 
 export default Profile;
